3dd22d35491c84fd7639551ff610b3feeb61cfec
jonathan leibovici <jonathanleibovici@gmail.com>
2020-09-16 13:39:57 -0400

    commit one file

---
+++ codePathAssignment_!/MovieCell.swift
@@ -0,0 +1,33 @@
+//
+//  MovieCell.swift
+//  codePathAssignment_!
+//
+//  Created by Jonathan Leibovici on 9/12/20.
+//  Copyright © 2020 jonathanleibovici. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
